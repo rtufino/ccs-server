dfe7dbfeb67acd8d67164a4f60a91b1b4c265745
pi <pi@raspberry>
2020-01-16 21:56:06 -0500

    Mejora scripts bash

--- respaldar.sh
+++ respaldar.sh
@@ -2,21 +2,21 @@
 
 # Script para obtener el respaldo de los registros del dia
 # Autor: Rodrigo Tufino <rtufino@lisoft.net>
-# Fecha: 11-01-2020
+# Version 1.0.1	11-01-2020
+# Version 1.0.2	16-01-2020
 
 fecha=`date +%F`
 hora=`date +%T`
 FILE_LOC=/tmp/respaldo.sql
-FILE_REM=`date "+%Y%m%d_%H%M%S"`.sql
+FILE_REM=`date "+%Y%m%d"`.sql
 FILE_LOG=respaldar.log
 
-mysqldump -u root -ph8SEWNe3FQC5dR db_ccs registro --where="fecha>='$fecha 00:00:00'" > $FILE_LOC
+mysqldump -u root -ph8SEWNe3FQC5dR db_ccs registro --where="fecha>='$fecha 00:00:00' and fecha<='$fecha 23:59:59'" > $FILE_LOC
 
 if test -f "$FILE_LOC"; then
-	scp -i /home/pi/.ssh/ccs-cloud-01 $FILE_LOC rtufino_lisoft_net@104.198.248.157:/home/rtufino_lisoft_net/respaldos/$FILE_REM
-	echo [`date "+%F %T"`] - COPIADO AL SERVIDOR >> $FILE_LOG
+        scp -i /home/pi/.ssh/ccs-cloud-01 $FILE_LOC rtufino_lisoft_net@104.198.248.157:/home/rtufino_lisoft_net/respaldos/$FILE_REM
+        echo [`date "+%F %T"`] - RESPALDO COPIADO AL SERVIDOR $FILE_REM >> $FILE_LOG
+        rm -f $FILE_LOC
 else
-	echo [`date "+%F %T"`] - NO SE CREO EL RESPALDO >> $FILE_LOG
+        echo [`date "+%F %T"`] - NO SE CREO EL RESPALDO >> $FILE_LOG
 fi
-
-rm -f $FILE_LOC
--- tunel.sh
+++ tunel.sh
@@ -12,7 +12,7 @@ np=`ps aux | grep $usuario | grep R$puerto | wc -l`
 if [ $np -eq 1 ]
 then
         ssh -f -N -T -R$puerto:127.0.0.1:22 -i /home/$usuario/.ssh/ccs-cloud-01 rtufino_lisoft_net@104.198.248.157
-        echo [`date "+%F %T"`] - CONECTADO >> $log
+        echo [`date "+%F %T"`] - RECONECCION >> $log
 else
         echo [`date "+%F %T"`] - EN EJECUCION >> $log
 fi
